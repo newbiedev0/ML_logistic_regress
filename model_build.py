c7e2e276a4299dffc8b3213699c22fce646d9b69
venkatesan <devopslearner216@gmail.com>
2025-11-05 10:19:22 +0530

    ok

--- model_build.py
+++ model_build.py
@@ -161,8 +161,8 @@ preprocessor = ColumnTransformer(
 
 # %%
 pipeline_attrition = ImbPipeline(steps=[('preprocessor', preprocessor),
-                               ('smote', smote_sampler),
-                               ('classifier', LogisticRegression(solver='liblinear', random_state=42))])
+                            ('smote', smote_sampler),
+                            ('classifier', LogisticRegression(solver='liblinear', random_state=42))])
 
 # %%
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, stratify=y)
